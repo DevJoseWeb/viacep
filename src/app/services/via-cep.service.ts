6776770d7735c50c61c1e79bc525955ecad71718
Junior <web2ajax@gmail.com>
2018-01-12 07:23:32 -0200

    //viacep.com.br/ws/RS/Porto Alegre/Domingos/json/
    //viacep.com.br/ws/RS/Porto Alegre/Domingos/json/

--- src/app/services/via-cep.service.ts
+++ src/app/services/via-cep.service.ts
@@ -10,8 +10,14 @@ export class ViaCepService {
 
   constructor(private httpClient: HttpClient) { }
 
+  //viacep.com.br/ws/01001000/json/
   getAddressByZipCode(zipcode: string): Observable<Address> {
     return this.httpClient.get<Address>(`https://viacep.com.br/ws/${zipcode}/json/`);
   }
+  //viacep.com.br/ws/RS/Porto Alegre/Domingos/json/ 
+  getAddressByName(uf,localidade,logradouro : string): Observable<Address> {
+    return this.httpClient.get<Address>(`https://viacep.com.br/ws/${uf}/${localidade}/${logradouro}/json/`);
+  }
 
 }
+
